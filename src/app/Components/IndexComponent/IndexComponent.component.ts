27bf17cc97e799e8a2086bbb87813b0d30d574f2
Mario <mario2002pc@hotmail.com>
2025-04-07 18:57:40 +0100

    New Branch

---
+++ src/app/Components/IndexComponent/IndexComponent.component.ts
@@ -0,0 +1,13 @@
+// src/app/pages/index/index.component.ts
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-IndexComponent',
+  templateUrl: './IndexComponent.component.html',
+  standalone: true,
+  // Asegúrate de que el nombre sea este
+  styleUrls: ['./IndexComponent.component.css']
+})
+export class IndexComponent {
+
+}
