723522c9e3b8d365f2102f2c977b959f36a490c2
enrique-eg <enrique.esgon@gmail.com>
2025-05-07 18:16:04 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sprint4',
+  webDir: 'www'
+};
+
+export default config;
